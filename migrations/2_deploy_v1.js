fe5f6bbb44a5d2b10d4c53970c7e08f252b8d62d
vincentbdb <inzaghia5@163.com>
2020-09-21 17:24:36 +0800

    config

--- migrations/2_deploy_v1.js
+++ migrations/2_deploy_v1.js
@@ -92,7 +92,7 @@ module.exports = async (deployer, network) => {
   // proxy will forward all the calls to the FiatTokenV1 impl
   const proxyAsV1 = await FiatTokenV1.at(FiatTokenProxy.address);
   await proxyAsV1.initialize(
-      "USD-GATE",
+      "GATE USD",
       "USDG",
       "USD",
       6,
