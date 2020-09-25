946e8bcd3cba7bb2cfe04d470c2c77107de20798
vincentbdb <inzaghia5@163.com>
2020-09-25 15:15:56 +0800

    update the script

--- scripts2/5.hdwallet.js
+++ scripts2/5.hdwallet.js
@@ -3,15 +3,28 @@ const contract = require("truffle-contract");
 const config = require("../config.js");
 const data = require("../build/contracts/FiatTokenV1.json");
 const network = 'rinkeby';
-const provider = new HDWalletProvider(config.MNEMONIC, `https://${network}.infura.io/v3/${config.INFURA_KEY}`,0,1);
+const provider = new HDWalletProvider(config.MNEMONIC, `https://${network}.infura.io/v3/${config.INFURA_KEY}`);
 
 function callContract() {
     const Storage = contract(data);
     Storage.setProvider(provider);
+    let proxyInstance;
     Storage.at(config.PROXY_CONTRACT_ADDRESS).then( function(instance) {
-        return instance.name.call();
-    }).then(result=>{
-        console.info(result.toString());
+        proxyInstance = instance;
+        return instance.totalSupply.call();
+    }).then(result=> {
+        console.info("before totalSupply:" + result.toString());
+        return proxyInstance.configureMinter("0x667030a0a91511aF534760c46351357b7356F375", 1000000, {from: "0x3574f849b6ffED10Ca661DD242770AF6048A78F8"});
+    }).then(result => {
+        return proxyInstance.minterAllowance("0x667030a0a91511aF534760c46351357b7356F375");
+    }).then(result => {
+        console.info("allowance:" + result.toString());
+        return proxyInstance.mint("0x3574f849b6ffED10Ca661DD242770AF6048A78F8","1000000",{from:"0x667030a0a91511aF534760c46351357b7356F375"});
+    }).then(result=> {
+        console.info("mint result:" + result);
+        return proxyInstance.totalSupply.call();
+    }).then(result=> {
+        console.info("after totalSupply:" + result.toString());
     }).catch(err=>{
         console.info(err.toString());
     });
@@ -24,7 +37,7 @@ function signTx() {
   console.info(web3.eth.accounts.sign("123",config.PRIVATE_KEY));
 }
 
-signTx();
+//signTx();
 callContract();
 
 provider.engine.stop();
